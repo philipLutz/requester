95576b50280133ff64276809fdaf9a637e92e74d
Philip Lutz <philiplutz413@gmail.com>
2020-02-22 10:51:27 -0800

    added .env and created reponse array

--- main.js
+++ main.js
@@ -3,18 +3,42 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const request = require('request');
+
+dotenv.config();
 const app = express();
 
-// Make URL and rate of requests a variables in .env
+app.use(express.json());
 
+let allResponses = [];
+
+app.get('/', (req, res) => {
+	return res.status(200).send({
+    'requestTarget': process.env.TARGETURL,
+    'totalResponses': allResponses.length,
+    'message': 'Made possible by the emotional support of Ricky Marine',
+    'repository': 'https://www.github.com/philipLutz/requester'
+   });
+});
+
+let response = null;
 async function makeRequests() {
   try {
-    await request('http://localhost:8080', function (error, response, body) {
-      console.error('error:', error);
-      console.log('statusCode:', response && response.statusCode);
+    await request(process.env.TARGETURL, function (error, response, body) {
+      response = {
+        error: error,
+        statusCode: response && response.statusCode,
+        timeStamp: Date.now()
+      };
+
+      allResponses.push(response);
+
+      console.log(response, `totalResponses: ${allResponses.length}`);
+
+      response = null;
+
       setTimeout(function() {
         makeRequests();
-      }, 1000);
+      }, (process.env.TIMEOUT*1000));
     });
   } catch(error) {
     console.log(error);
@@ -23,7 +47,11 @@ async function makeRequests() {
     }, 1000);
   }
 }
-console.log('Requester running');
+
+app.listen(process.env.PORT);
+console.log(
+  `Requesting ${process.env.TARGETURL} and listening on port ${process.env.PORT}`
+);
 
 makeRequests();
 
