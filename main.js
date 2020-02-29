75b1c65f368a6674f586eae3772feafeb720fbd6
Philip Lutz <philiplutz413@gmail.com>
2020-02-28 16:32:44 -0800

    changed port and fixed response object

--- main.js
+++ main.js
@@ -30,23 +30,20 @@ function timeout(ms) {
 async function makeRequests() {
 	while (allResponses.length < process.env.LIMIT) {
 		let response = {
-			statusCode: null,
+			status: null,
 			date: null,
-			message: null
 		};
 		await Promise.all([
 			rp(process.env.TARGETURL)
 			.then(function(res) {
-				response.statusCode = res.statusCode;
-				response.date = res.response.headers.date;
-				response.message = res.message;
+				response.status = 'success';
+				response.date = new Date();
 				console.log(response);
 				allResponses.push(response);
 			})
-			.catch(function(res) {
-				response.statusCode = res.statusCode;
-				response.date = res.response.headers.date;
-				response.message = res.message;
+			.catch(function(err) {
+				response.status = 'error';
+				response.date = Date.now();
 				console.log(response);
 				allResponses.push(response);
 			}),
@@ -57,9 +54,11 @@ async function makeRequests() {
 	console.log('Requests complete');
 }
 
-app.listen(process.env.PORT);
+const PORT = process.env.PORT || 80;
+
+app.listen(PORT);
 console.log(
-  `Requesting ${process.env.TARGETURL} and listening on port ${process.env.PORT}`
+  `Requesting ${process.env.TARGETURL} and listening on port ${PORT}`
 );
 
 makeRequests();
